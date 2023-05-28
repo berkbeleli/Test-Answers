4433384a47b7447bd380ec3102c2c4df1db018f3
Berk <berkbeleli@gmail.com>
2023-05-29 00:07:39 +0300

    Adds First Answer

---
+++ Answer 1/RoomService/RoomService.WebAPI/PasswordCheckerMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace RoomService.WebAPI
+{
+    public class PasswordCheckerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public PasswordCheckerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Check if the request contains the header "passwordKey" with the value "passwordKey123456789"
+            string passwordKey = context.Request.Headers["passwordKey"];
+
+            if (IsValidPasswordKey(passwordKey))
+            {
+                // Allow the request to proceed to the next middleware
+                await _next(context);
+            }
+            else
+            {
+                // Return HTTP response code 403 Forbidden
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+            }
+        }
+
+        private bool IsValidPasswordKey(string passwordKey)
+        {
+            const string expectedPasswordKey = "passwordKey123456789";
+
+            // Compare the password key with the expected value
+            return !string.IsNullOrEmpty(passwordKey) && passwordKey == expectedPasswordKey;
+        }
+
+    }
+}
